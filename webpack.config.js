c8478eb0c9b53dc6ec0cc09c3104126cddb335e8
PedroHenrique-O <pedrokaz.henrique@gmail.com>
2025-06-02 13:51:48 -0300

    Production fix: Environment-aware Module Federation config for dev/prod

--- webpack.config.js
+++ webpack.config.js
@@ -2,57 +2,64 @@ const { ModuleFederationPlugin } = require("webpack").container;
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { withZephyr } = require("zephyr-webpack-plugin");
 
-const baseConfig = {
-  entry: "./src/index.ts",
-  mode: "development",
-  devServer: {
-    port: 3000,
-    open: true,
-    historyApiFallback: true,
-    hot: true,
-  },
-  output: {
-    publicPath: "auto",
-    clean: true,
-  },
-  resolve: {
-    extensions: [".tsx", ".ts", ".jsx", ".js"],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|tsx|js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-react", "@babel/preset-typescript"],
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === "production";
+
+  const baseConfig = {
+    entry: "./src/index.ts",
+    mode: argv.mode || "development",
+    devServer: {
+      port: 3000,
+      open: true,
+      historyApiFallback: true,
+      hot: true,
+    },
+    output: {
+      publicPath: "auto",
+      clean: true,
+    },
+    resolve: {
+      extensions: [".tsx", ".ts", ".jsx", ".js"],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(ts|tsx|js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-react", "@babel/preset-typescript"],
+            },
           },
         },
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"],
+        },
+      ],
+    },
+    plugins: [
+      new ModuleFederationPlugin({
+        name: "host",
+        remotes: isProduction
+          ? ["remotetodo"] // Zephyr production - array format
+          : {
+              // Development - localhost URLs
+              remotetodo: "remotetodo@http://localhost:3001/remoteEntry.js",
+            },
+        shared: {
+          react: { singleton: true },
+          "react-dom": { singleton: true },
+          "react-router-dom": { singleton: true },
+        },
+      }),
+      new HtmlWebpackPlugin({
+        template: "./public/index.html",
+        title: "Module Federation Todo App",
+      }),
     ],
-  },
-  plugins: [
-    new ModuleFederationPlugin({
-      name: "host",
-      remotes: {
-        remotetodo: "remotetodo@[remotetodoUrl]/remoteEntry.js",
-      },
-      shared: {
-        react: { singleton: true },
-        "react-dom": { singleton: true },
-        "react-router-dom": { singleton: true },
-      },
-    }),
-    new HtmlWebpackPlugin({
-      template: "./public/index.html",
-      title: "Module Federation Todo App",
-    }),
-  ],
-};
+  };
 
-module.exports = withZephyr()(baseConfig);
+  return withZephyr()(baseConfig);
+};
